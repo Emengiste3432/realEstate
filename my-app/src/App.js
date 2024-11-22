19c7f222a7b3b2583d0ab721d68d7b1530eb1248
Emengiste3432 <emanuelmengiste@gmail.com>
2024-11-22 11:31:44 -0600

    my commit

---
+++ my-app/src/App.js
@@ -0,0 +1,52 @@
+import "./App.css";
+import React, { useState, useEffect } from 'react';
+
+
+
+function App() {
+
+ const [info, setInfo] = useState([]);
+  useEffect(() => {
+    fetch("/api/items")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network does not work")
+      }
+      return response.json();
+    })
+    .then(info => {
+      setInfo(info);
+    })
+
+    .catch(error => {
+      console.error("Error fetching data, ", error);
+      }
+  )},
+  []);
+
+  return (
+    <div className="App">
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {info.map((data) => {
+            return (
+              <tr>
+                <td>{data.id}</td>
+                <td>{data.data}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+   }
+
+
+export default App;
