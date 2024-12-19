5e6add4ed8c6a80e1eb2bdcf0d0bb50c902eeafd
nagebeyaw <nagebeyaw@senecafoundation.org>
2024-12-19 11:31:47 -0600

    committing changes

--- api/server.js
+++ api/server.js
@@ -1,10 +1,19 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 const PORT = process.env.PORT || 3500;
 
+
+import bodyParser from 'body-parser';
+
+
+// Middleware to parse JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Optional: to handle URL-encoded data
+
+
 // Import routes
-const routes = require('./index'); // Adjust path as necessary
+import routes from './index.js'; // Adjust path as necessary
 app.use('/api', routes);
 
 // Start the server and store the instance in the 'server' variable
